ff78ad71db5bdf9c2e0f66b696c0d05ab5b1284a
MithunKumarRajak <mithunkumarrajak01012005@gmail.com>
2025-11-25 01:44:07 +0530

    Category Improved

--- category/models.py
+++ category/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
     category_name = models.CharField(max_length=100)
     # Slug field for URL-friendly representation
     slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
     category_image = models.ImageField(
         upload_to='categories/', blank=True, null=True)
 
