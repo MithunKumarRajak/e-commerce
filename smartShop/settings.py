f677cf69084ecda9c6be39524040731afcc830cd
MithunKumarRajak <mithunkumarrajak01012005@gmail.com>
2025-11-24 12:20:02 +0530

    Static setting updated

--- smartShop/settings.py
+++ smartShop/settings.py
@@ -127,19 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# medis files configuration
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
-#
+# media files configuration
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # for custom user
 AUTH_USER_MODEL = 'accounts.Account'
