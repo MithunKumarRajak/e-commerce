47d59bc5aa934470a7bb444fb420d0b3e16332c7
MithunKumarRajak <148649863+MithunKumarRajak@users.noreply.github.com>
2026-05-21 10:17:43 +0530

    Use decouple config to load Twilio settings

--- smartShop/settings.py
+++ smartShop/settings.py
@@ -191,8 +191,6 @@ if not EMAIL_BACKEND:
 
 
 # sms configuration through twilio
-import os
-
-TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
-TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+TWILIO_ACCOUNT_SID = config("TWILIO_ACCOUNT_SID", default=None)
+TWILIO_AUTH_TOKEN = config("TWILIO_AUTH_TOKEN", default=None)
+TWILIO_PHONE_NUMBER = config("TWILIO_PHONE_NUMBER", default=None)
